de6229ddc9b1c350bb86eaf205d8c9926e4bb757
Jinu Noh <yes.jinu@gmail.com>
2024-10-05 16:25:44 +0900

    refactor: split status bar manager into a class

--- main.ts
+++ main.ts
@@ -1,185 +1,189 @@
-import { randomInt } from 'crypto';
-import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
+import { App, Notice, Plugin, PluginSettingTab, Setting } from "obsidian";
 
 interface BrainDumpSettings {
-	backspaceDisabled: boolean;
-	runnerModeEnabled: boolean;
+  backspaceDisabled: boolean;
+  runnerModeEnabled: boolean;
 }
 
 const DEFAULT_SETTINGS: BrainDumpSettings = {
-	backspaceDisabled: false,
-	runnerModeEnabled: false,
+  backspaceDisabled: false,
+  runnerModeEnabled: false,
+};
+
+class StatusBarManager {
+  el: HTMLElement | undefined;
+
+  constructor(el: HTMLElement) {
+    this.el = el;
+  }
+
+  resetView() {
+    this.updateStatusBar(`---------------------------`);
+  }
+
+  updateView(tpm: number) {
+    this.updateStatusBar(`Typing Speed: ${tpm} TPM`);
+  }
+
+  private updateStatusBar(text: string) {
+    this.el?.setText(text);
+  }
 }
 
 export default class BrainDumpMode extends Plugin {
-	settings: BrainDumpSettings;
-	statusBarItemEl: HTMLElement;
-	lastSnapshot: string | undefined = undefined;
-	wordTimestamps: number[] = [];
+  settings: BrainDumpSettings;
+  statusBarManager: StatusBarManager;
+  lastSnapshot: string | undefined = undefined;
+  wordTimestamps: number[] = [];
+  isBrainDumpModeOn = false;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    this.settings = { ...DEFAULT_SETTINGS, ...(await this.loadData()) };
 
-		const ribbonIconEl = this.addRibbonIcon('brain', 'Brain Dump Mode', (evt: MouseEvent) => {
-			this.toggleBrainDumpMode();
-		});
-		ribbonIconEl.addClass('brain-dump-ribbon-class');
+    // status bar
+    this.statusBarManager = new StatusBarManager(this.addStatusBarItem());
 
-		this.statusBarItemEl = this.addStatusBarItem();
-		this.updateStatusBar();
+    // ribbon
+    this.addRibbonIcon(
+      "train-front", // https://lucide.dev/icons/
+      "Activate Brain Dump Mode",
+      (evt: MouseEvent) => {
+        this.toggleBrainDumpMode();
+      }
+    );
 
-		this.addCommand({
-			id: 'toggle-brain-dump-mode',
-			name: 'Toggle',
-			callback: () => {
-				this.toggleBrainDumpMode();
-			}
-		});
+    // command palette
+    this.addCommand({
+      id: "toggle-brain-dump-mode",
+      name: "Activate",
+      callback: () => {
+        this.toggleBrainDumpMode();
+      },
+    });
 
-		this.addSettingTab(new BrainDumpSettingTab(this.app, this));
+    // setting tab
+    this.addSettingTab(new BrainDumpSettingTab(this.app, this));
 
+    // register 'keydown' event listeners
+    this.registerDomEvent(document, "keydown", (evt: KeyboardEvent) => {
+      if (this.settings.runnerModeEnabled) {
+        const currentTime = Date.now();
+        this.wordTimestamps.push(currentTime);
+      }
 
-		this.registerDomEvent(document, 'keydown', (evt: KeyboardEvent) => {
-			if (this.settings.runnerModeEnabled) {
-				const currentTime = Date.now();
-				this.wordTimestamps.push(currentTime);
-			}
+      if (this.settings.backspaceDisabled) {
+        if (evt.key === "ArrowLeft" || evt.key === "ArrowUp") {
+          // Disable moving cursor to left or up
+          this.app.workspace.activeEditor?.editor?.setCursor(
+            Number.MAX_SAFE_INTEGER
+          );
+          this.noticeBrainDumpModeIsOn();
+          return;
+        }
 
-			if (this.settings.backspaceDisabled) {
-				if (evt.key === 'ArrowLeft' || evt.key === 'ArrowUp') {
-					// Disable moving cursor to left or up
-					this.app.workspace.activeEditor?.editor?.setCursor(Number.MAX_SAFE_INTEGER)
-					this.alertBrainDumpModeIsOn();
-					return;
-				}
+        if (evt.key === "Backspace" || evt.key === "Delete") {
+          // Disable backspace and delete by recovering last snapshot
+          this.app.workspace.activeEditor?.editor?.setValue(
+            `${this.lastSnapshot}`
+          );
+          this.app.workspace.activeEditor?.editor?.setCursor(
+            Number.MAX_SAFE_INTEGER
+          );
+          this.noticeBrainDumpModeIsOn();
+          return;
+        }
 
-				if (evt.key === 'Backspace' || evt.key === 'Delete') {
-					// Disable backspace and delete by recovering last snapshot
-					this.app.workspace.activeEditor?.editor?.setValue(`${this.lastSnapshot}`)
-					this.app.workspace.activeEditor?.editor?.setCursor(Number.MAX_SAFE_INTEGER)
-					this.alertBrainDumpModeIsOn();
-					return;
-				}
+        // Save content snapshot every key press
+        const lastWord = evt.key === "Enter" ? "" : evt.key;
+        this.lastSnapshot = `${this.app.workspace.activeEditor?.editor?.getValue()}${lastWord}`;
+      }
+    });
 
-				// Save content snapshot every key press
-				const lastWord = (evt.key === 'Enter') ? '' : evt.key
-				this.lastSnapshot = `${this.app.workspace.activeEditor?.editor?.getValue()}${lastWord}`
-			}
-		});
+    // register interval for calculating typing speed
+    this.registerInterval(
+      window.setInterval(() => {
+        if (this.settings.runnerModeEnabled) {
+          const tpm = this.calculateTPM();
+          this.statusBarManager.updateView(tpm);
+        }
+      }, 100)
+    );
+  }
 
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			// FIXME: Trigger only when click on editor
-			if (this.settings.backspaceDisabled) {
-				this.app.workspace.activeEditor?.editor?.setCursor(Number.MAX_SAFE_INTEGER)
-				this.alertBrainDumpModeIsOn();
-			}
-		})
+  calculateTPM(): number {
+    const targetWindow = 5000;
+    const targetWindowSec = targetWindow / 1000;
+    const multipleForMinute = 60 / targetWindowSec;
+    const from = Date.now() - targetWindow;
+    this.wordTimestamps = this.wordTimestamps.filter(
+      (timestamp) => timestamp > from
+    );
+    const wordsInLastSeconds = this.wordTimestamps.length;
 
-		this.registerInterval(
-			window.setInterval(() => {
-				if (this.settings.runnerModeEnabled) {
-					this.calculateRecentWPT();
-				}
-			}, 100)
-		);
+    return wordsInLastSeconds * multipleForMinute;
+  }
 
-	}
+  noticeBrainDumpModeIsOn() {
+    new Notice(`Brain Dump Mode 🔥 Backspace DISABLED`);
+  }
 
-	calculateRecentWPT() {
-		const SLIDING_WINDOW = 5000
-		const SLIDING_WINDOW_IN_SECONDS = SLIDING_WINDOW / 1000;
-		const MULTIIPLE = 60 / SLIDING_WINDOW_IN_SECONDS;
-		const sometimeAgo = Date.now() - SLIDING_WINDOW;
-		this.wordTimestamps = this.wordTimestamps.filter(timestamp => timestamp > sometimeAgo);
-		const wordsInLastSeconds = this.wordTimestamps.length;
-		this.statusBarItemEl.setText(`Typing Speed: ${wordsInLastSeconds * MULTIIPLE} TPM`);
-	}
+  toggleBrainDumpMode() {
+    this.isBrainDumpModeOn = !this.isBrainDumpModeOn;
+    this.settings.backspaceDisabled = this.isBrainDumpModeOn;
+    this.settings.runnerModeEnabled = this.isBrainDumpModeOn;
+    if (!this.isBrainDumpModeOn) {
+      this.statusBarManager.resetView();
+    }
+    this.saveSettings();
+    this.showNoticeTurnedOn();
+  }
 
-	alertBrainDumpModeIsOn() {
-		const messages = [
-			`Brain Dump Mode is ON 🔥`,
-			// `There's no going back. Move FORWARD!`,
-			// `You can do this!`,
-			// TODO: Get from user
-		]
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 
-		new Notice(this.randomSelect(messages));
-	}
-
-	randomSelect(arr: string[]): string {
-		return arr[randomInt(0, arr.length)]
-	}
-
-	onunload() {
-		// Clean up any resources if needed
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	toggleBrainDumpMode() {
-		this.settings.backspaceDisabled = !this.settings.backspaceDisabled;
-		this.settings.runnerModeEnabled = !this.settings.runnerModeEnabled;
-		this.saveSettings();
-		this.updateStatusBar();
-		this.displayNotice();
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-
-	updateStatusBar() {
-		this.statusBarItemEl.setText(`Brain Dump Mode ${this.settings.backspaceDisabled ? '✅' : '❌'}`);
-	}
-
-	displayNotice() {
-		const status = this.settings.backspaceDisabled ? '✅' : '❌';
-		new Notice(`Brain Dump Mode ${status}`);
-	}
+  showNoticeTurnedOn() {
+    new Notice(
+      `Brain Dump Mode ${this.settings.backspaceDisabled ? "✅" : "❌"}`
+    );
+  }
 }
 
 class BrainDumpSettingTab extends PluginSettingTab {
-	plugin: BrainDumpMode;
+  plugin: BrainDumpMode;
 
-	constructor(app: App, plugin: BrainDumpMode) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: BrainDumpMode) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const { containerEl } = this;
+  display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Enable brain dump mode')
-			.setDesc("If turned on, delete key will be disabled")
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.backspaceDisabled)
-				.onChange(async (value) => {
-					this.plugin.settings.backspaceDisabled = value;
-					await this.plugin.saveSettings();
-					this.plugin.updateStatusBar();
-					this.plugin.displayNotice();
-				}
-				)
-			);
+    new Setting(containerEl)
+      .setName("Enable brain dump mode")
+      .setDesc("If turned on, delete key will be disabled")
+      .addToggle((toggle) =>
+        toggle
+          .setValue(this.plugin.settings.backspaceDisabled)
+          .onChange(async (value) => {
+            this.plugin.settings.backspaceDisabled = value;
+            this.plugin.saveSettings();
+          })
+      );
 
-		new Setting(containerEl)
-			.setName('Enable runner mode')
-			.setDesc("If turned on, runner mode enabled")
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.runnerModeEnabled)
-				.onChange(async (value) => {
-					this.plugin.settings.runnerModeEnabled = value;
-					await this.plugin.saveSettings();
-					this.plugin.updateStatusBar();
-					this.plugin.displayNotice();
-				}
-				)
-			);
-	}
+    new Setting(containerEl)
+      .setName("Enable runner mode")
+      .setDesc("If turned on, runner mode enabled")
+      .addToggle((toggle) =>
+        toggle
+          .setValue(this.plugin.settings.runnerModeEnabled)
+          .onChange(async (value) => {
+            this.plugin.settings.runnerModeEnabled = value;
+            this.plugin.saveSettings();
+          })
+      );
+  }
 }
